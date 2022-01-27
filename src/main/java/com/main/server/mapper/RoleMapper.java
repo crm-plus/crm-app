cbfe7235900d39963166ae3cd61bf71f424df2ce
DimaGalchenko <galchenkodima31@gmail.com>
2022-01-27 22:34:55 +0200

    Add UserDTO and RoleDTO. Add appropriate mappers for dtos

--- src/main/java/com/main/server/mapper/RoleMapper.java
+++ src/main/java/com/main/server/mapper/RoleMapper.java
@@ -1,10 +1,15 @@
 package com.main.server.mapper;
 
+import com.main.server.dto.RoleDTO;
+import com.main.server.entity.Role;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
 @Mapper
 public interface RoleMapper {
     RoleMapper INSTANCE = Mappers.getMapper(RoleMapper.class);
 
-    RoleDTO roleToDTO(Role);
+    RoleDTO roleToDTO(Role role);
 
-    User roleToUser(RoleDTO);
+    Role roleToUser(RoleDTO roleDTO);
 }
--- src/main/java/com/main/server/mapper/UserMapper.java
+++ src/main/java/com/main/server/mapper/UserMapper.java
@@ -1,12 +1,15 @@
 package com.main.server.mapper;
 
+import com.main.server.dto.UserDTO;
+import com.main.server.entity.User;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    UserDTO usertoDTO(User);
+    UserDTO userToDTO(User user);
 
-    User dtoToUser(UserDTO);
+    User dtoToUser(UserDTO userDTO);
 }
