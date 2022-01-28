fd5118eb0feffb913cc14e42510269ce230b65db
DimaGalchenko <galchenkodima31@gmail.com>
2022-01-28 21:57:09 +0200

    Create Role and User Repository. Create db init files and update application.properties

--- src/main/java/com/main/server/dao/UserDAO.java
+++
@@ -1,4 +0,0 @@
-package com.main.server.dao;
-
-public interface UserDAO {
-}
---
+++ src/main/java/com/main/server/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.main.server.repository;
+
+import com.main.server.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
