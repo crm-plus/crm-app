2101be380faaa5eb3173a1e747677b0249ee51be
Yevhenii <malakhov747@gmail.com>
2022-07-25 17:41:10 +0300

    Change name

--- crm-apis/src/main/java/com/main/server/service/interfaces/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.main.server.service.interfaces;
-
-import com.main.server.dto.UserRequest;
-import com.main.server.exception.ResourceNotFoundException;
-
-public interface AccountService {
-
-    void processRegister(UserRequest userRequest) throws ResourceNotFoundException;
-}
--- crm-apis/src/test/java/com/example/mainserver/util/TestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.mainserver.util;
-
-public class TestException extends RuntimeException {
-    public TestException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
