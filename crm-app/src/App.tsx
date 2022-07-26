98b15e4bb7b590275d0bc9749c252574e4d443d5
DimaGalchenko <galchenkodima31@gmail.com>
2022-07-26 20:07:43 +0300

    added header component

--- crm-app/src/App.tsx
+++ crm-app/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './component/header/Header'
+
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+})
+
+const lightTheme = createTheme({
+    palette: {
+        mode: 'light',
+    },
+})
+
+const isDarkMode = true
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme }>
+          <CssBaseline />
+          <div className='App'>
+              <Header />
+          </div>
+      </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
