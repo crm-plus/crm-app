2f3eb753d13cf7ad076de0d437994f0f3e4af74b
DimaGalchenko <galchenkodima31@gmail.com>
2022-10-21 22:47:47 +0300

    Update user schema

--- src/constant/ApiEndpoint.ts
+++ src/constant/ApiEndpoint.ts
@@ -1,7 +1,8 @@
 const PROTOCOL = 'http://';
-const HOST = 'localhost';
+//const HOST = 'localhost';
+const HOST = 'crm-plus-api.eu-central-1.elasticbeanstalk.com/';
 const PORT = '8888';
-
-const API_URL = `${PROTOCOL}${HOST}:${PORT}/api`;
+const API_URL = `${PROTOCOL}${HOST}/api`;
+//const API_URL = `${PROTOCOL}${HOST}:${PORT}/api`;
 
 export default API_URL;
